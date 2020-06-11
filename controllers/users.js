20efad386d6150de1177a8e2b05031969bdde191
Janos Mezo <jmezo.asd@gmail.com>
2020-06-11 09:34:57 +0200

    set fields to send on get users endpoint

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,6 @@ const { validationResult } = require('express-validator');
 const User = require('../models/user');
 
 exports.getUsers = async (req, res, next) => {
-  const users = await User.find();
+  const users = await User.find({}, '_id email name role');
   res.status(200).json(users);
 }
