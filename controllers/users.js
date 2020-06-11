6d3ecb20de70b467bbeddee08580aae585aac174
Janos Mezo <jmezo.asd@gmail.com>
2020-06-11 16:09:41 +0200

    added put endpoint for user

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,40 @@
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
 exports.getUsers = async (req, res, next) => {
   const users = await User.find({}, '_id email name role');
   res.status(200).json(users);
+}
+
+exports.updateUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('validation failed');
+    error.statusCode = 422;
+    error.data = errors.array();
+    return next(error);
+  }
+  const user = await User.findById(req.body.userId);
+  const email = req.body.email || user.email;
+  const name = req.body.name || user.name;
+  const password = encryptPassword(req.body.password) || user.password;
+  const role = req.body.role || user.role;
+  try {
+    await User.updateOne({_id: req.body.userId}, {email: email, name: name, password: password, role: role});
+    res.status(200).json({message: 'user updated', userId: user._id.toString()});
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode(500);
+    }
+    next(err);
+  }
+}
+
+
+const encryptPassword = (pw) => {
+  if (!pw) return undefined;
+  const salt = bcrypt.genSaltSync();
+  return bcrypt.hashSync(pw, salt);
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const usersController = require('../controllers/users');
+const User = require('../models/user');
 
 const router = express.Router();
 
 router.get('/users', usersController.getUsers);
+router.put(
+  '/user',
+  [
+    body('userId')
+      .custom((value) => {
+        return User.findById(value)
+        .then((user) => {
+          if (user) {
+            Promise.reject('not a valid user id');
+          }
+        });
+      }),
+    body('email')
+      .if(body('email').exists())
+      .isEmail().withMessage('not a valid email')
+      .custom((value) => {
+        return User.findOne({ email: value })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('email already taken');
+          }
+        });
+      })
+  ],
+  usersController.updateUser
+  );
 
 module.exports = router;
