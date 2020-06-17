e054b9e52b978c46ce760ff2720fadfa505a1a2f
Janos Mezo <jmezo.asd@gmail.com>
2020-06-17 10:33:31 +0200

    added headers for handling cors

--- rest-api/app.js
+++ rest-api/app.js
@@ -15,6 +15,16 @@ app.use((_, __, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/auth', authRoutes);
 app.use('/users', authGuard, usersRoutes);
 
